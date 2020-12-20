9def329c957d66ba5c95489730097952e3c3dd06
zemiguel20 <migli.coutinho@gmail.com>
2020-12-20 22:49:24 +0000

    #4 Define Vehicle - service implemented

--- Services/VehicleService.cs
+++ Services/VehicleService.cs
@@ -1,14 +1,30 @@
 using System.Threading.Tasks;
 using lapr5_masterdata_viagens.DTOs;
 using lapr5_masterdata_viagens.Shared;
+using lapr5_masterdata_viagens.Domain;
+using lapr5_masterdata_viagens.Mappers;
+using lapr5_masterdata_viagens.Repositories;
 
 namespace lapr5_masterdata_viagens.Services
 {
     public class VehicleService
     {
+        private readonly IVehicleRepo _repo;
+
+        public VehicleService(IVehicleRepo repo)
+        {
+            this._repo = repo;
+        }
+
         public async Task<Result<VehicleDTO>> CreateVehicle(VehicleDTO dto)
         {
-            return Result<VehicleDTO>.Ok(dto);
+            var result = Vehicle.Create(dto);
+            if (result.IsSuccess == false)
+                return Result<VehicleDTO>.Fail(result.Error);
+
+            Vehicle vehicleSaved = await _repo.Save(result.Value);
+
+            return Result<VehicleDTO>.Ok(VehicleMapper.ToDto(vehicleSaved));
         }
     }
 }
