79b452751b683103c9ea1d3f18da0cd1cf0e393f
zemiguel20 <migli.coutinho@gmail.com>
2020-12-20 17:02:18 +0000

    #4 Define Vehicle - vehicle dto added

---
+++ DTOs/VehicleDto.cs
@@ -0,0 +1,11 @@
+namespace lapr5_masterdata_viagens.DTOs
+{
+    public class VehicleDTO
+    {
+        public string Id { get; set; }
+        public string CarPlateCode { get; set; }
+        public string VIN { get; set; }
+        public string ServiceStartDate { get; set; }
+        public string VehicleTypeID { get; set; }
+    }
+}
