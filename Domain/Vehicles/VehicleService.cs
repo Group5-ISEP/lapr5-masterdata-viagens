fb6e937316192286c6545545aa0c4b37ce096c8d
zemiguel20 <migli.coutinho@gmail.com>
2020-12-21 01:50:47 +0000

    #4 Define Vehicle - service now has unit of work

--- Domain/Vehicles/VehicleService.cs
+++ Domain/Vehicles/VehicleService.cs
@@ -1,14 +1,17 @@
 using System.Threading.Tasks;
 using lapr5_masterdata_viagens.Shared;
+using lapr5_masterdata_viagens.Domain.Shared;
 
 namespace lapr5_masterdata_viagens.Domain.Vehicles
 {
     public class VehicleService
     {
         private readonly IVehicleRepo _repo;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public VehicleService(IVehicleRepo repo)
+        public VehicleService(IVehicleRepo repo, IUnitOfWork unitOfWork)
         {
+            this._unitOfWork = unitOfWork;
             this._repo = repo;
         }
 
@@ -18,7 +21,9 @@ namespace lapr5_masterdata_viagens.Domain.Vehicles
             if (result.IsSuccess == false)
                 return Result<VehicleDTO>.Fail(result.Error);
 
-            Vehicle vehicleSaved = await _repo.Save(result.Value);
+            Vehicle vehicleSaved = await _repo.AddAsync(result.Value);
+
+            await _unitOfWork.CommitAsync();
 
             return Result<VehicleDTO>.Ok(VehicleMapper.ToDto(vehicleSaved));
         }
