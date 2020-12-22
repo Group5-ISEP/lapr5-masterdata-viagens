fc9a4d9d026bb9b6138477687c9597219d2f441a
zemiguel20 <migli.coutinho@gmail.com>
2020-12-22 17:37:09 +0000

    #4 Define Vehicle - vehicle schema config

--- Infrastructure/ViagensDbContext.cs
+++ Infrastructure/ViagensDbContext.cs
@@ -11,5 +11,44 @@ namespace lapr5_masterdata_viagens.Infrastructure
         }
 
         public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Write Fluent API configurations here
+
+            //Property Configurations
+
+            ConfigureVehicles(modelBuilder);
+        }
+
+        private void ConfigureVehicles(ModelBuilder modelBuilder)
+        {
+            //ID
+            modelBuilder.Entity<Vehicle>()
+                .HasKey(v => v.Id);
+
+            //Plate code
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.CarPlateCode)
+                .IsRequired();
+            modelBuilder.Entity<Vehicle>()
+                .HasIndex(v => v.CarPlateCode)
+                .IsUnique();
+
+            //VIN
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.VIN)
+                .IsRequired();
+
+            //Service Start Date
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.ServiceStartDate)
+                .IsRequired();
+
+            //Type id
+            modelBuilder.Entity<Vehicle>()
+                .Property(v => v.VehicleTypeID)
+                .IsRequired();
+        }
     }
 }
