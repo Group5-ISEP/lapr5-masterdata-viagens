8fb8b67c095c61e0e05eed6b214ecc3ee626d948
zemiguel20 <migli.coutinho@gmail.com>
2020-12-27 22:20:47 +0000

    #6 Create Trips - Migrations updated

---
+++ Viagens.API/Migrations/20201227221918_TripsAdded.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace lapr5_masterdata_viagens.Migrations
+{
+    public partial class TripsAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    PathID = table.Column<string>(type: "TEXT", nullable: true),
+                    LineID = table.Column<string>(type: "TEXT", nullable: true),
+                    Orientation = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PassingTimes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeInstant = table.Column<int>(type: "INTEGER", nullable: false),
+                    NodeID = table.Column<string>(type: "TEXT", nullable: true),
+                    TripId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PassingTimes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PassingTimes_Trips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "Trips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PassingTimes_TripId",
+                table: "PassingTimes",
+                column: "TripId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PassingTimes");
+
+            migrationBuilder.DropTable(
+                name: "Trips");
+        }
+    }
+}
