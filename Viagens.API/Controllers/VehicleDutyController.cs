a8b2193d9d96781036f6a4aa967c648e72a472d2
zemiguel20 <migli.coutinho@gmail.com>
2021-01-03 17:01:36 +0000

    #8 Create Vehicle Duty - controller added

---
+++ Viagens.API/Controllers/VehicleDutyController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using lapr5_masterdata_viagens.Domain.VehicleDuties;
+using System.Collections.Generic;
+
+namespace lapr5_masterdata_viagens.Controllers
+{
+    [ApiController]
+    [Route("api/vehicleduty")]
+    public class VehicleDutyController : ControllerBase
+    {
+        private readonly VehicleDutyService _service;
+
+        public VehicleDutyController(VehicleDutyService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VehicleDutyDTO>> Create(VehicleDutyDTO dto)
+        {
+            var result = await _service.CreateVehicleDuty(dto);
+            if (result.IsSuccess == false)
+            {
+                return Conflict(result.Error);
+            }
+            return Created("api/vehicleduty/" + result.Value.Id, result.Value);
+        }
+    }
+}
